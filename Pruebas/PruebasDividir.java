d5f957bea9e3636dce24342090562d4f98ce073c
Victor <v.condeortiz@gmail.com>
2024-03-13 22:21:37 +0100

    PruebasDividir.java

---
+++ Pruebas/PruebasDividir.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PruebasDividir {
+    @Test
+    void testDividir() {
+
+        Division dividir = new Division();
+
+
+        double num1 = 2.0;
+        double num2 = 3.0;
+
+
+        double resultado = dividir.Dividir();
+        assertEquals(num1 / num2, resultado);
+
+    }
+
+
+        @Test
+        void testDividir1() {
+
+            Division dividir = new Division();
+
+
+            double num1 = 5.0;
+            double num2 = 3.0;
+
+
+            double resultado = dividir.Dividir();
+            assertEquals(num1 / num2, resultado);
+
+        }
+
+
+            @Test
+            void testDividir2() {
+
+                Division dividir = new Division();
+
+
+                double num1 = 4.0;
+                double num2 = 3.0;
+
+
+                double resultado = dividir.Dividir();
+                assertEquals(num1 / num2, resultado);
+
+            }
+        }
+
+
+
+
+
