2fc01aed5bdd860d3448462adfa5176c1f6cacc8
Victor <v.condeortiz@gmail.com>
2024-03-13 21:12:27 +0100

    Resta.java

---
+++ src/Resta.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Resta {
+    public double Restar( ) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce dos números para restar: ");
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        return num1 - num2;
+    }
+}
