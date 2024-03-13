1838a320bfba40f7810156037fc45e29abc64df0
Victor <v.condeortiz@gmail.com>
2024-03-13 21:31:54 +0100

    Division

---
+++ src/Division.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Division {
+    public double Dividir() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce dos números para dividir: ");
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        if (num2 == 0) {
+            return 0;
+        } else {
+            return num1 / num2;
+        }
+    }
+}
